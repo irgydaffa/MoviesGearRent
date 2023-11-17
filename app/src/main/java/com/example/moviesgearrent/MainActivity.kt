6207ae4d4c5c26536ee9c1e41d1361dc67123124
irgydaffa <irgytl12@gmail.com>
2023-11-17 12:45:27 +0700

    tes

--- app/src/main/java/com/example/moviesgearrent/MainActivity.kt
+++ app/src/main/java/com/example/moviesgearrent/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent{
             SignUp()
-//            punya irgy
+
         }
 
             }
