45e305756cb41caaa6b24bd1ec9202d39e37ee05
irgydaffa <irgytl12@gmail.com>
2023-11-17 12:38:41 +0700

    First Commit

--- app/src/main/java/com/example/moviesgearrent/MainActivity.kt
+++ app/src/main/java/com/example/moviesgearrent/MainActivity.kt
@@ -10,37 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.moviesgearrent.App.SignUp
 import com.example.moviesgearrent.ui.theme.MoviesGearRentTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MoviesGearRentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContent{
+            SignUp()
+        }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoviesGearRentTheme {
-        Greeting("Android")
-    }
-}
