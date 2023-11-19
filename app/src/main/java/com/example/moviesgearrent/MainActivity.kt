d24486244180607fd95bd3aaf637f292f1a6fc35
irgydaffa <irgytl12@gmail.com>
2023-11-19 12:38:17 +0700

    commit progres

--- app/src/main/java/com/example/moviesgearrent/MainActivity.kt
+++ app/src/main/java/com/example/moviesgearrent/MainActivity.kt
@@ -1,23 +1,77 @@
 package com.example.moviesgearrent
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.moviesgearrent.App.SignUp
-import com.example.moviesgearrent.ui.theme.MoviesGearRentTheme
-
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            SignUp()
+            val sharedPreferences: SharedPreferences = LocalContext.current.getSharedPreferences("auth", Context.MODE_PRIVATE)
+            val navController = rememberNavController()
+
+            val startDestination = String
+            var jwt = sharedPreferences.getString ("jwt","")
+            if(jwt.equals("")){
+                startDestination = "greeting"
+            }else if (){
+                startDestination = "dashboard"
+            }
+
+            NavHost(navController, startDestination = startDestination){
+                composable (route = "greeting"){
+                    Greeting(navController)
+                }
+                composable (route = "dashboard"){
+                    dashboard(navController)
+                }
+                composable (route = "register"){
+                    register(navController)
+                }
+                composable (
+                    route = "edituserpage/{userid}/{username}",
+                ) {backStackEntry ->
+
+                    EditUserPage(navController, backStackEntry.arguments?.getString("userid"), backStackEntry.arguments?.getString("username"))
+                )
+            }
 
         }
 
