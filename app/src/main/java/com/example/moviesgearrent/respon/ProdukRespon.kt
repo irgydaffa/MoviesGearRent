aa2fd20b717a492bffc25b47b1a1230827aef889
irgydaffa <irgytl12@gmail.com>
2023-12-21 16:54:53 +0700

    progress get data homepage

---
+++ app/src/main/java/com/example/moviesgearrent/respon/ProdukRespon.kt
@@ -0,0 +1,11 @@
+package com.example.moviesgearrent.respon
+
+class HomeRespon {
+    var id: Int? = null
+    var name: String? = null
+    var price: Int? = null
+    var description: String? = null
+    var image: String? = null
+    var created_at: String? = null
+    var updated_at: String? = null
+}
---
+++ app/src/main/java/com/example/moviesgearrent/service/HomeService.kt
@@ -0,0 +1,7 @@
+package com.example.moviesgearrent.service
+
+import retrofit2.http.POST
+
+interface HomeData {
+    @POST
+}
